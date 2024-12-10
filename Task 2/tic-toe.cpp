4ffc5be2927023ded13bbf04ce9cce30e8cc72d9
Bhupendra Singh <bs4004908@gmail.com>
2024-12-10 20:11:47 +0530

    calci

--- Task 2/tic-toe.cpp
+++ Task 2/tic-toe.cpp
@@ -1,23 +1,17 @@
 #include<iostream>
 using namespace std;
 
-int space[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+char space[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
 int row;
 int column;
 char token = 'x';
-
+bool tie;
+string n1=" ";
+ string n2=" ";
 
 void functionOne(){
     
-     string n1;
-    string n2;
-    cout << " Enter the name of the first player: \n";
-    getline(cin, n1);
-    cout << "Enter the name of the Second player : \n";
-    getline(cin, n2);
-    cout<< n1 << " is player1 so he/she will play first \n ";
-    cout<< n2 << " is player2 so he/she will play second \n ";
- 
+
     cout << "   |      |   \n";
     cout << " " <<space[0][0]<< "  |  "<<space[0][1]<<"   |  "<<space[0][2]<<"   \n";
     cout << " ___|______|____\n";
@@ -40,7 +34,7 @@ if(token == 'x'){
 }
 
 
-if(tolen == '0'){
+if(token == '0'){
     cout<< n2 <<"please enter";
     cin>>digit;   
 }
@@ -108,5 +102,66 @@ else{
     cout << "There is no empty space! "<< endl;
     functionTwo();
 }
+ functionOne();
 }
 
+
+
+bool functionThree(){
+  for(int i=0;i<3;i++)
+  {
+     if(space[i][0] == space[i][1] && space[i][0] == space[i][2] || space[0][i] == space[1][i] && space[0][i] == space[2][i])
+     return true;
+  }
+  if(space[0][0] == space[1][1] && space[1][1] == space[2][2] || space[0][2] == space[1][1] &&  space[1][1] == space[2][0])
+  {
+     return true;
+  }
+
+for(int i=0;i<3;i++)
+{
+  for(int j=0;j<3;j++)
+  {
+    if (space[i][j] != 'x' && space[i][j] != '0')
+    {
+    return false;
+          }
+      }
+    
+    }
+     tie = true;
+    return false;
+}
+
+
+
+int main(){
+
+cout << " Enter the name of the first player: \n";
+    getline(cin, n1);
+    cout << "Enter the name of the Second player : \n";
+    getline(cin, n2);
+    cout<< n1 << " is player1 so he/she will play first \n ";
+    cout<< n2 << " is player2 so he/she will play second \n ";
+
+    while(!functionThree())
+    {
+       functionOne();
+       functionTwo();
+       functionThree();
+
+    }
+    if(token == 'x' && tie == false)
+    {
+        cout << n1 <<"Wins!!" << endl;
+    }
+    else if(token == '0' && tie == false)
+    {
+        cout << n2 <<"Wins!!" <<endl;
+    }
+    else
+    {
+         cout << "It's a Draw!" << endl;
+    }
+
+}
